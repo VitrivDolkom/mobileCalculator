c1eadda579ca012d5531db5428c5c2043e7fb258
Volkov Dmitriy <vdy0555@gmail.com>
2023-02-19 16:04:01 +0700

    number button listeners and erase button functional

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -18,9 +18,25 @@ class MainActivity : AppCompatActivity() {
 
         val expressionText: TextView = binding.expression.expressionResultText
 
-        binding.buttons.buttonOne.setOnClickListener{
-            val newExpressionText = expressionText.text.toString() + "1"
-            expressionText.text = newExpressionText
-        }
+        binding.expression.buttonErase.setOnClickListener{this.eraseSymbol(expressionText)}
+
+        binding.buttons.buttonZero.setOnClickListener{this.addToViewText(expressionText, '0')}
+        binding.buttons.buttonOne.setOnClickListener{this.addToViewText(expressionText, '1')}
+        binding.buttons.buttonTwo.setOnClickListener{this.addToViewText(expressionText, '2')}
+        binding.buttons.buttonThree.setOnClickListener{this.addToViewText(expressionText, '3')}
+        binding.buttons.buttonFour.setOnClickListener{this.addToViewText(expressionText, '4')}
+        binding.buttons.buttonFive.setOnClickListener{this.addToViewText(expressionText, '5')}
+        binding.buttons.buttonSix.setOnClickListener{this.addToViewText(expressionText, '6')}
+        binding.buttons.buttonSeven.setOnClickListener{this.addToViewText(expressionText, '7')}
+        binding.buttons.buttonEight.setOnClickListener{this.addToViewText(expressionText, '8')}
+        binding.buttons.buttonNine.setOnClickListener{this.addToViewText(expressionText, '9')}
+    }
+
+    private fun eraseSymbol(view: TextView) {
+        view.text = view.text.toString().replaceFirst(".$".toRegex(), "")
+    }
+
+    private fun addToViewText(view: TextView, symbolToAdd: Char) {
+        view.text = view.text.toString() + symbolToAdd
     }
 }
