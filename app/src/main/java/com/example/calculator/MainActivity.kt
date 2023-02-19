7b78971269fd881f025d7d04f493f6361ddd2fe3
Volkov Dmitriy <vdy0555@gmail.com>
2023-02-19 20:05:38 +0700

    changed kotlin and xml code style settings

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -6,32 +6,29 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
     private val ce = CalculateExpression()
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-
-
+        
         setContentView(view)
     }
-
+    
     fun eraseSymbol(it: View) {
         var expressionText = binding.expression.expressionResultText.text
-
+        
         binding.expression.expressionResultText.text = expressionText.toString().replaceFirst(".$".toRegex(), "")
     }
-
+    
     fun onButtonsClick(it: View) {
         val btn = it as Button
-
-        val newText =  binding.expression.expressionResultText.text.toString() + btn.text
+        
+        val newText = binding.expression.expressionResultText.text.toString() + btn.text
+        
         binding.expression.expressionResultText.text = newText
     }
-
 }
