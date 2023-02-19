c439e4ba05de81fcb7523aa21c7f4366fa3741b8
Volkov Dmitriy <vdy0555@gmail.com>
2023-02-19 18:59:40 +0700

    rewrited button click listeners to single function

--- app/src/main/java/com/example/calculator/CalculateExpression.kt
+++ app/src/main/java/com/example/calculator/CalculateExpression.kt
@@ -2,5 +2,15 @@ package com.example.calculator
 
 class CalculateExpression {
 
+    fun calculate(actionSymbol: Char, expressionText1: String, expressionText2: String): Int {
+        val expression1 = expressionText1.toInt()
+        val expression2 = expressionText2.toInt()
 
+        when(actionSymbol) {
+            '+' -> return expression1 + expression2
+        }
+
+        return expression1
+    }
 }
+
--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.calculator
 
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
 
@@ -9,35 +10,28 @@ import com.example.calculator.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val ce = CalculateExpression()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+
+
         setContentView(view)
-
-        val expressionText: TextView = binding.expression.expressionResultText
-
-        binding.expression.buttonErase.setOnClickListener{this.eraseSymbol(expressionText)}
-
-        binding.buttons.buttonZero.setOnClickListener{this.addToViewText(expressionText, '0')}
-        binding.buttons.buttonOne.setOnClickListener{this.addToViewText(expressionText, '1')}
-        binding.buttons.buttonTwo.setOnClickListener{this.addToViewText(expressionText, '2')}
-        binding.buttons.buttonThree.setOnClickListener{this.addToViewText(expressionText, '3')}
-        binding.buttons.buttonFour.setOnClickListener{this.addToViewText(expressionText, '4')}
-        binding.buttons.buttonFive.setOnClickListener{this.addToViewText(expressionText, '5')}
-        binding.buttons.buttonSix.setOnClickListener{this.addToViewText(expressionText, '6')}
-        binding.buttons.buttonSeven.setOnClickListener{this.addToViewText(expressionText, '7')}
-        binding.buttons.buttonEight.setOnClickListener{this.addToViewText(expressionText, '8')}
-        binding.buttons.buttonNine.setOnClickListener{this.addToViewText(expressionText, '9')}
     }
 
-    private fun eraseSymbol(view: TextView) {
-        view.text = view.text.toString().replaceFirst(".$".toRegex(), "")
+    fun eraseSymbol(it: View) {
+        var expressionText = binding.expression.expressionResultText.text
+
+        binding.expression.expressionResultText.text = expressionText.toString().replaceFirst(".$".toRegex(), "")
     }
 
-    private fun addToViewText(view: TextView, symbolToAdd: Char) {
-        val newText = view.text.toString() + symbolToAdd
-        view.text = newText
+    fun onButtonsClick(it: View) {
+        val btn = it as Button
+
+        val newText =  binding.expression.expressionResultText.text.toString() + btn.text
+        binding.expression.expressionResultText.text = newText
     }
+
 }
