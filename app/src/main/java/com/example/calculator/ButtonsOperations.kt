61b2147e15d606b3f6dd59c050de6f96f79554ec
Volkov Dmitriy <vdy0555@gmail.com>
2023-03-02 09:28:53 +0700

    operations enum

---
+++ app/src/main/java/com/example/calculator/ButtonsOperations.kt
@@ -0,0 +1,5 @@
+package com.example.calculator
+
+enum class ButtonsOperations(val operation: String) {
+    PLUS("+"), MINUS("-"), SIGN_CHANGE("±"), MULTIPLICATION("×"), COMMA("."), EQUAL("="), PERCENT("%"), DIVIDE("÷")
+}
